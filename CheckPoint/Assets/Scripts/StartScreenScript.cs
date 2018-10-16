c19a3bd852dc113020eb846b5d53c8d89ffaa32d
Daniel Boyd <dan_boyd20@hotmail.com>
2018-10-16 16:25:53 +0100

    Added Theme music, Made level 4 level 2, removed old level 2

--- CheckPoint/Assets/Scripts/StartScreenScript.cs
+++ CheckPoint/Assets/Scripts/StartScreenScript.cs
@@ -14,6 +14,9 @@ public class StartScreenScript : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.Return))
         {
             GameObject.Find("FadeImage").GetComponent<FadeScript>().StartFade("Level 1", 3.0f);
+            GameObject audio = GameObject.Find("ThemeMusic");
+            DontDestroyOnLoad(audio);
+            audio.GetComponent<AudioSource>().PlayDelayed(3.0f);
         }
 	}
 }
