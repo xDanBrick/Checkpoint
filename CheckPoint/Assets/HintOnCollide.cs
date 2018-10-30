0700502cb47b3027f1ec3c26ccc5b1c896834789
Daniel Boyd <dan_boyd20@hotmail.com>
2018-10-30 16:04:04 +0000

    Added Collide on hint

---
+++ CheckPoint/Assets/HintOnCollide.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HintOnCollide : MonoBehaviour {
+
+    [SerializeField] string hintText = "Hint";
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            GameObject.Find("Hint Text").GetComponent<Text>().text = hintText;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            GameObject.Find("Hint Text").GetComponent<Text>().text = "";
+        }
+    }
+}
