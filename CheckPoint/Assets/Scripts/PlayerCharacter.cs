f3f52b5fa4410e81e2ad51648d9ca21b79f118c0
Daniel Boyd <dan_boyd20@hotmail.com>
2018-11-01 15:53:27 +0000

    Revert "Added death animation logic"
    This reverts commit 031f02fdc5ad61f7f2b08e30f3ec59b088562ccc.

--- CheckPoint/Assets/Scripts/PlayerCharacter.cs
+++ CheckPoint/Assets/Scripts/PlayerCharacter.cs
@@ -213,8 +213,7 @@ public class PlayerCharacter : MonoBehaviour
                     ghost.transform.position = transform.position;
                     ghost.GetComponent<GhostScript>().PlayerIsDead();
                     GameObject.FindGameObjectWithTag("MainCamera").GetComponent<FollowPlayer>().setTransformFollow(FollowPlayer.TransformFollow.Ghost);
-                    //GetComponent<SpriteRenderer>().enabled = false;
-                    m_Anim.SetTrigger("Death");
+                    GetComponent<SpriteRenderer>().enabled = false;
                 }
                 canMovePlayer = false;
                 
@@ -234,7 +233,7 @@ public class PlayerCharacter : MonoBehaviour
         m_PlayerHead.Translate(new Vector3(0.0f, 1.0f, 0.0f));
         m_PlayerHead.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
         m_PlayerHead.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, 250.0f));
-        bodyRespawnDelay = 1.5f;
+        bodyRespawnDelay = 2.0f;
         GetComponent<SpriteRenderer>().enabled = true;
 
         transform.position = currentSpawnPosition;
