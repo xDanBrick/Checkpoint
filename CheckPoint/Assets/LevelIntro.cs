d3504145de7b3318655945b558f24c26b8749e88
Daniel Boyd <dan_boyd20@hotmail.com>
2018-11-05 16:33:29 +0000

    Added Level select functionallity

---
+++ CheckPoint/Assets/LevelIntro.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelIntro : MonoBehaviour {
+
+    // Use this for initialization
+	void Start () {
+        GetComponent<Text>().text = "Level " + (LevelStats.currentLevel + 1).ToString() + " - " + LevelStats.levelIntros[LevelStats.currentLevel];
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Level " + (LevelStats.currentLevel + 1).ToString());
+        }
+    }
+}
---
+++ CheckPoint/Assets/LevelSelect.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+class LevelStats
+{
+    public static string[] levelIntros = new string[5] {
+        "The Night Begins",
+        "The Night Is the best",
+        "",
+        "",
+        ""
+    };
+    public static int currentLevel = 0;
+}
+
+public class LevelSelect : MonoBehaviour {
+
+    Transform indicator;
+    int buttonIndex = 0;
+    bool mainMenu = false;
+	// Use this for initialization
+	void Start () {
+        indicator = transform.Find("Indicator");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if(buttonIndex < 4)
+            {
+                RectTransform trans = indicator.GetComponent<RectTransform>();
+                trans.localPosition = new Vector3(trans.localPosition.x, trans.localPosition.y - 60, trans.localPosition.z);
+                GameObject.Find("Level " + (buttonIndex + 1).ToString() + " Text").GetComponent<Text>().color = Color.black;
+                ++buttonIndex;
+                GameObject.Find("Level " + (buttonIndex + 1).ToString() + " Text").GetComponent<Text>().color = Color.red;
+            }
+            else if(!mainMenu)
+            {
+                mainMenu = true;
+                GameObject.Find("Level " + (buttonIndex + 1).ToString() + " Text").GetComponent<Text>().color = Color.black;
+                RectTransform trans = indicator.GetComponent<RectTransform>();
+                trans.localPosition = new Vector3(-200.0f, -345.0f, trans.localPosition.z);
+                GameObject.Find("Menu Text").GetComponent<Text>().color = Color.red;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) && buttonIndex > 0)
+        {
+            if (mainMenu)
+            {
+                RectTransform trans = indicator.GetComponent<RectTransform>();
+                trans.localPosition = new Vector3(-300.0f, -280.0f, trans.localPosition.z);
+                GameObject.Find("Menu Text").GetComponent<Text>().color = Color.black;
+                GameObject.Find("Level " + (buttonIndex + 1).ToString() + " Text").GetComponent<Text>().color = Color.red;
+                mainMenu = false;
+            }
+            else
+            {
+                RectTransform trans = indicator.GetComponent<RectTransform>();
+                trans.localPosition = new Vector3(trans.localPosition.x, trans.localPosition.y + 60, trans.localPosition.z);
+                GameObject.Find("Level " + (buttonIndex + 1).ToString() + " Text").GetComponent<Text>().color = Color.black;
+                --buttonIndex;
+                GameObject.Find("Level " + (buttonIndex + 1).ToString() + " Text").GetComponent<Text>().color = Color.red;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            LevelStats.currentLevel = buttonIndex;
+            UnityEngine.SceneManagement.SceneManager.LoadScene("LevelIntro");
+            //if (buttonIndex == 0)
+            //{
+            //    gameObject.SetActive(false);
+            //    Time.timeScale = 1.0f;
+            //}
+            //else if (buttonIndex == 1)
+            //{
+            //    gameObject.SetActive(false);
+            //    Time.timeScale = 1.0f;
+            //    GameObject.Find("FadeImage").GetComponent<FadeScript>().StartFade(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name, 3.0f);
+            //}
+            //else if (buttonIndex == 2)
+            //{
+            //    gameObject.SetActive(false);
+            //    Time.timeScale = 1.0f;
+            //    UnityEngine.SceneManagement.SceneManager.LoadScene("StartScene");
+            //}
+        }
+    }
+}
