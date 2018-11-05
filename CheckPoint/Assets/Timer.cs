8f8877e267126bada715c02d47b70177e5717c5d
Daniel Boyd <dan_boyd20@hotmail.com>
2018-11-05 11:31:23 +0000

    Revert "Revert "Merge branch 'Develop' of https://github.com/xDanBrick/Checkpoint into Develop""
    This reverts commit cc1ec90841934c5c3e1a0b36221d2401ebfd65a1.

---
+++ CheckPoint/Assets/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    private Text timerText;
+    private float timer = 0.0f;
+
+
+	// Use this for initialization
+	void Start () {
+        timerText = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        timerText.text = timer.ToString("f1");
+    }
+}
