9999ba63c6f93e74d7ac613d9d16b96eca469b84
Daniel Boyd <dan_boyd20@hotmail.com>
2018-11-07 16:07:01 +0000

    Added menu music

--- CheckPoint/Assets/Scripts/GhostScript.cs
+++ CheckPoint/Assets/Scripts/GhostScript.cs
@@ -7,16 +7,20 @@ public class GhostScript : MonoBehaviour {
     public bool playerIsDead = false;
     private Transform player;
     [SerializeField] [Range(1.0f, 7.0f)] private float moveSpeed = 3.0f;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+
+    private AudioSource ghostAudio;
+    void Start () {
         player = GameObject.Find("TestHead").transform;
-	}
+        ghostAudio = GameObject.Find("GhostAudio").GetComponent<AudioSource>();
+
+    }
 	
     public void PlayerIsDead()
     {
         playerIsDead = true;
         transform.localScale = new Vector3(player.position.x > transform.position.x ? 1.0f : -1.0f, transform.localScale.y, transform.localScale.z);
-
+        ghostAudio.Play();
         // get the angle
         //Vector3 norTar = (player.position - transform.position).normalized;
         //float angle = Mathf.Atan2(norTar.y, norTar.x) * Mathf.Rad2Deg;
@@ -37,6 +41,7 @@ public class GhostScript : MonoBehaviour {
                 GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCharacter>().RespawnBody();
                 GetComponent<SpriteRenderer>().enabled = false;
                 GameObject.FindGameObjectWithTag("MainCamera").GetComponent<FollowPlayer>().setTransformFollow(FollowPlayer.TransformFollow.Player);
+                ghostAudio.Stop();
             }
         }
 	}
